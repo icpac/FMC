33f297d2110f5beed2e5442cec74fe76633ceb6c
icpac <cj.icpac@gmail.com>
2021-10-05 06:18:00 -0500

    Matriz Elemental Inversa

---
+++ Git/MatrizElementalInversa.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+""" Por inspección encontrar la matriz inversa de una 
+matriz elemental
+"""
+
+__autor__ = "Tlacaelel Icpac"
+__email__ = "tlacaelel.icpac@gmail.com"
+
+import numpy as np
+
+a = np.array(
+    [
+        [0, 1],
+        [1, 0]
+    ]
+)
+
+def PrintMul(a, b):
+    print("Matriz:\n", a) 
+    print("Producto: \n", np.matmul(a, b))
+
+b = np.array(
+    [
+        [1, 0],
+        [5, 1]
+    ]
+)
+
+binv = np.array(
+    [
+        [1, 0],
+        [-5, 1]
+    ]
+)
+
+c = np.array(
+    [
+        [1, 0, 0],
+        [0, 1, -3],
+        [0, 0, 1]
+    ]
+)
+
+cinv = np.array(
+    [
+        [1, 0, 0],
+        [0, 1, 3],
+        [0, 0, 1]
+    ]
+)
+
+PrintMul(a, a)
+PrintMul(b, binv)
+PrintMul(c, cinv)
